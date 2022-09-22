39ee59df12a3102d2814c7e0820c797a7b3b1ee7
minimo-io <nicolas@minimo.io>
2022-09-22 15:14:23 -0300

    mo home ready!

--- mo-home.js
+++ mo-home.js
@@ -1,13 +1,40 @@
-{/* <a href="#"><i class="" aria-hidden="true"></i> Leer más</a> */}
-
-
-const app = createApp({
-    data() {
-      return {
-        readMoreText: "Leer más",
-        readMoreIcon: "fa fa-arrow-right",
-      }
-    }
-  })
+jQuery(document).ready(function(){
   
-app.mount('#mo-home-vue-app')
+  var READMORELANG, READLESSLANG;
+  const app = Vue.createApp({
+      data() {
+          return {
+              readMoreActive: false,
+              readMoreText: "Leer más",
+              readMoreIcon: "fa fa-arrow-right",
+          }
+      },
+      mounted(){
+          READMORELANG = jQuery(".readMoreBtn").data("readmorelang");
+          READLESSLANG = jQuery(".readMoreBtn").data("readlesslang");
+
+
+      },
+      methods:{
+          readMore(){
+              if (this.readMoreActive){
+                  this.readMoreText = READMORELANG;
+                  this.readMoreIcon = "fa fa-arrow-right";
+
+              }else{
+                  this.readMoreText = READLESSLANG;
+                  this.readMoreIcon = "fa fa-minus";
+              }
+              this.readMoreActive = ! this.readMoreActive;
+
+              jQuery(".d-none").toggle("fast", function(){
+                  
+              });
+
+
+          }
+      }
+  })
+  app.mount('#mo-home-vue-app')
+
+});
