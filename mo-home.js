de0bec251b59928c135aeb94f210cb9da12cbcf3
minimo-io <nicolas@minimo.io>
2022-09-22 14:17:35 -0300

    test page added

--- mo-home.js
+++ mo-home.js
@@ -1,7 +1,13 @@
-const App = new Vue({
-    el: "#button-vue",
-    data: {
-        buttonTextInactive: "Leer más",
-        buttonTextActive: "Leer menos"
+{/* <a href="#"><i class="" aria-hidden="true"></i> Leer más</a> */}
+
+
+const app = createApp({
+    data() {
+      return {
+        readMoreText: "Leer más",
+        readMoreIcon: "fa fa-arrow-right",
+      }
     }
-});
+  })
+  
+app.mount('#mo-home-vue-app')
