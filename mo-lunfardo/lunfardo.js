e0e16b01e5537141e3c672847e2606d690c40916
minimo-io <nicolas@minimo.io>
2022-10-19 14:00:54 -0300

    migrated to vuejs 3

---
+++ mo-lunfardo/lunfardo.js
@@ -0,0 +1,50 @@
+const App = Vue.createApp({
+  data(){
+    return {
+      placeholder: 'Buscá che...',
+      searchQuery: '',
+      wordsJson: {
+        'word-1':  { "title": "sonamos", "description": "pop. ¡Estamos perdidos!"},
+        'word-2': { "title": "percanta", "description": "(pop.) Mujer (LCV.), amante (LCV.), querida (LCV.), concubina."},
+      }
+    }
+  },
+  methods:{
+    matches(obj) {
+      const term = this.searchQuery.toLowerCase();
+      return obj.title.toLowerCase().includes(term) || obj.description.toLowerCase().includes(term);
+    }
+  },
+
+  mounted: {
+
+      // const res = fetch("");
+
+  },
+  
+  computed:{
+      listValues() {
+        return Object.values(this.wordsJson);
+      },
+      countWords(){
+        return Object.keys(this.wordsJson).length;
+      },
+      filteredList() {
+        if (!this.searchQuery) {
+          return this.listValues;
+        }
+
+        return this.listValues
+          .map((v) => {
+            if (this.matches(v)) {
+              return v;
+            }
+          })
+          .filter((v) => v);
+      }
+    },
+
+  }).mount("#app");
+
+
+  
