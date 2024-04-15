5fed66133dfa4d582836df555726e0ee4eed171e
minimo-io <nicolas@minimo.io>
2024-04-15 20:01:50 -0300

    Starting out nostr followback

---
+++ nostr-followback/README.md
@@ -0,0 +1,10 @@
+# Nostr Reciprocal Follow
+
+Check who does and does not follow you back on Nostr.
+
+## ToDo
+
+-   Create this but without SvelteKit (just Vite + Svelte)
+-   Polish the proof-of-concept code
+-   Configure Vite for the miniapp to be loaded in the article's url as base
+-   Save followbackers and not followbackers in lists to see details
---
+++ nostr-followback/src/routes/+page.svelte
@@ -0,0 +1,121 @@
+<script>
+    // Import the package
+    import NDK from "@nostr-dev-kit/ndk";
+
+    let npubToQuery = "npub1wujhdsytm3w6g0mpsqh8v7ezx83jcm64dlkwuqgm5v8lv0pds55ssudkw0";
+    let userName;
+    let userThumb;
+    let followsCount;
+
+    let followBackCount = 0;
+    let notFollowBackCount = 0;
+    let unknownFollowBack = 0;
+
+    async function fetchUserProfile(npub, ndk) {
+        const user = ndk.getUser({ npub });
+        await user.fetchProfile();
+        // console.log("User profile:", user.profile);
+        // console.log("User profile:", user);
+        return user;
+    }
+    async function fetchNotes(hexkey, ndk) {
+        const kind1filter = { kinds: [3], authors: [hexkey] };
+        return ndk.fetchEvent(kind1filter);
+
+        // const kind1filter = { author: [hexkey] };
+        // return ndk.fetchEvent(kind1filter);
+    }
+
+    async function bootstrap() {
+        try {
+            const ndk = new NDK({
+                explicitRelayUrls: [
+                    "wss://relay.hodl.ar",
+                    "wss://relay.current.fyi",
+                    "wss://nostr.wine",
+                    "wss://nostr.plebchain.org",
+                    "wss://purplepag.es",
+                    "wss://nos.lol",
+                    "wss://nostr.mom",
+                    "wss://nostrelay.yeghro.site",
+                    "wss://relay.damus.io",
+                    "wss://relay.nostr.bg",
+                    "wss://relay.snort.social",
+                    "wss://relay.primal.net",
+                    "wss://nostr.bitcoiner.social",
+                    "wss://nostr.mutinywallet.com",
+                    "wss://relay.current.fyi",
+                    "wss://nostr-pub.wellorder.net",
+
+                    // "wss://brb.io",
+                    // "wss://eden.nostr.land",
+                    // "wss://nostr.orangepill.dev",
+                ],
+            });
+            await ndk.connect();
+
+            // const user = await fetchUserProfile("npub1l8hja34gyeqrhc8plpcl3sql476n2rkgrzexazsfytwjkmy9kndqhx6pk9", ndk);
+            const user = await fetchUserProfile(npubToQuery, ndk);
+
+            userName = user.profile.name;
+            userThumb = user.profile.image;
+
+            if (userName) {
+                // console.log(user.profile);
+                const follows = await user.follows();
+
+                followsCount = follows.size;
+
+                follows.forEach(async (follower) => {
+                    await new Promise((resolve) => setTimeout(resolve, 1000));
+                    const followerFollowList = await follower.follows();
+
+                    if (followerFollowList.size) {
+                        // console.log(followerFollowList);
+                        // check if the user is in the queried user follow list
+                        let doesFollowBack = false;
+                        for (const contact of followerFollowList) {
+                            if (contact.npub == npubToQuery) {
+                                doesFollowBack = true;
+                                break;
+                            }
+                        }
+                        if (doesFollowBack) {
+                            followBackCount++;
+                        } else {
+                            notFollowBackCount++;
+                        }
+                    } else {
+                        unknownFollowBack++;
+                    }
+                });
+            }
+
+            // console.log("Followers:");
+            // let followers = await fetchNotes(user.hexpubkey, ndk);
+            // console.log(followers.tags.length);
+
+            // const longNotes = await fetchLongNotes(hexkey, ndk);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+
+    bootstrap();
+</script>
+
+<h1>Who does not follows you on Nostr</h1>
+{#if !userThumb}
+    {npubToQuery}
+{/if}
+<hr />
+{#if userThumb}
+    <div class="user-box">
+        <img src={userThumb} width="50" style="border-radius:100%;" alt="user-thumb" />
+        User: {userName} |  Follows: {followsCount}
+        <br />
+        Unknown: {unknownFollowBack} | Follow_Back: {followBackCount} | Not_Follow_Back: {notFollowBackCount}
+    </div>
+{:else}
+    <div class="loader">Loading data...</div>
+{/if}
